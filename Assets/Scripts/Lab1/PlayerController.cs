803ae0348f1c7c366ee9d60883e01143eb860fcd
zymxer <ilyayan1756@gmail.com>
2023-10-18 19:47:12 +0300

    Lab1

---
+++ Assets/Scripts/Lab1/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement parameters")]
+    [Range(0.01f, 20.0f)]
+    [SerializeField] 
+    private float moveSpeed = 0.1f; // moving speed of the player
+    [Space(10)]
+    [Range(0.01f, 20.0f)]
+    [SerializeField]
+    private float jumpForce = 0.1f;
+
+    public float rayLength = 2.0f;
+
+    private Rigidbody2D _rigidbody;
+
+    public LayerMask groundLayer;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        Debug.DrawRay(transform.position, Vector3.down * rayLength, Color.white);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("jumping");
+        }
+    }
+
+}
