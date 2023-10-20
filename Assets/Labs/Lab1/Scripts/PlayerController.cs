ddd1c7c53acdb85f802cde33c91d6398e84de046
zymxer <ilyayan1756@gmail.com>
2023-10-21 01:12:32 +0200

    Lab 1 dz do IV

--- Assets/Labs/Lab1/Scripts/PlayerController.cs
+++ Assets/Labs/Lab1/Scripts/PlayerController.cs
@@ -16,11 +16,16 @@ public class PlayerController : MonoBehaviour
     public float rayLength = 2.0f;
 
     private Rigidbody2D _rigidbody;
-
+    private Animator _animator;
+    private bool _isWalking = false;
+    private bool _isFacingRight = true;
+    private Vector3 _scale;
     public LayerMask groundLayer;
 
     private void Awake()
     {
+        _scale = transform.localScale;
+        _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -33,19 +38,54 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
+            if(!_isFacingRight)
+            {
+                _scale.x *= -1;
+                transform.localScale = _scale;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            if (_isFacingRight)
+            {
+                _scale.x *= -1;
+                transform.localScale = _scale;
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            _isFacingRight = true;
+            _isWalking = true;
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            _isFacingRight = false;
+            _isWalking = true;
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         }
-        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            _isWalking = false;
+            transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            _isWalking = false;
+        }
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
         Debug.DrawRay(transform.position, Vector3.down * rayLength, Color.white);
+        _animator.SetBool("isGrounded", IsGrounded());
+        _animator.SetBool("isWalking", _isWalking);
+        if(!_isFacingRight)
+        {
+
+        }
     }
 
     private bool IsGrounded()
